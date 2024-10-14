b50844775704352944ec706ac8825fc836334ccd
Владислав Якимук <Tyuiu.YakimukVV@outlook.com>
2024-10-14 23:13:16 +0500

    Task5

--- Tyuiu.YakimukVV.Sprint2.Task5.V9/Program.cs
+++ Tyuiu.YakimukVV.Sprint2.Task5.V9/Program.cs
@@ -6,14 +6,14 @@ namespace Tyuiu.YakimukVV.Sprint2.Task5.V9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите номер месяца (от 1 до 12):");
+            Console.WriteLine("Введите число:");
             int month = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите число:");
+            Console.WriteLine("Введите номер месяца (от 1 до 12):");
             int day = int.Parse(Console.ReadLine());
 
             DataService dataService = new DataService();
-            string nextDate = dataService.FindDateOfNextDay(month, day);
+            string nextDate = dataService.FindDateOfNextDay(day, month);
 
             Console.WriteLine($"Следующая дата: {nextDate}");
         }
